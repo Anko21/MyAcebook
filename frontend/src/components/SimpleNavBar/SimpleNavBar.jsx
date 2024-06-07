652b92847b8412b282ddc53998ee25fa9edab4ea
Anko21 <am.kontaratou@gmail.com>
2024-06-07 13:30:24 +0100

    added a login logout btn

--- frontend/src/components/SimpleNavBar/SimpleNavBar.jsx
+++ frontend/src/components/SimpleNavBar/SimpleNavBar.jsx
@@ -1,8 +1,31 @@
+import { useLocation, Link } from 'react-router-dom';
+import './SimpleNavBar.css';
+
 export const SimpleNavBar= () => {
+    const location = useLocation();
+
     return (
-        <div className="heading-banner">
-            <h1>Acebook</h1>
-            <h3>Brought to you by the Agile Avengers</h3>
-      </div>
+        <div className="simple-navbar">
+            <div className="heading-banner">
+                <h1>Acebook</h1>
+                <h3>Brought to you by the Agile Avengers</h3>
+            </div>
+            <div className='signup-login'>
+                {location.pathname !== '/signup' 
+                    && location.pathname !== '/posts' 
+                        && location.pathname !== '/' &&(
+                    <h3>
+                        <button className="signup-login-btn"><Link to="/signup">Signup</Link></button>
+                    </h3> 
+                )}
+                {location.pathname !== '/login' && 
+                    location.pathname !== '/posts' && 
+                        location.pathname !== '/' &&( 
+                    <h3>
+                        <button className="signup-login-btn"><Link to="/login">Login</Link></button>
+                    </h3>
+                )}
+            </div>
+        </div>
     )
 }
